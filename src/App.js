850efd576b9140ce12466d1de51cd93705373aa1
Hai Nguyen <hainguyen@ip-10-1-9-98.ec2.internal>
2020-02-27 16:24:44 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, {useRef, useReducer, useCallback} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+    const inputRef = useRef()
+    const [items, dispatch] = useReducer((state, action) => {
+        switch (action.type) {
+            case "add":
+                return [...state, {name: action.name, id: state.length}]
+            case "remove":
+                return state.filter(item => item.id !== action.id)
+            default:
+                return state
+        }
+    }, [])
 
-    </div>
-  );
+    const handleSubmit = useCallback(e => {
+        e.preventDefault()
+        if (inputRef.current.value !== "") {
+            dispatch({type: "add", name: inputRef.current.value})
+            inputRef.current.value = ""
+        }
+    }, [])
+
+    return (
+        <div style={{padding: "20px"}}>
+            <form onSubmit={handleSubmit}>
+                <input
+                    ref={inputRef}
+                    placeholder="Input item name and press enter..."
+                    style={{minWidth: "250px"}}
+                />
+            </form>
+            <ul>
+                {items.map(item => {
+                    return (
+                        <li key={item.id}>{item.name} &nbsp;
+                            <button key={item.id} onClick={() => dispatch({type: "remove", id: item.id})}>remove
+                            </button>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
