9e93d76a832ccf730d793c71b47c31edab6371b3
Hai Nguyen <hainguyen@ip-10-1-9-59.ec2.internal>
2020-02-28 10:13:15 -0500

    added clear list button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, {useRef, useReducer, useCallback} from 'react';
+import React, { useRef, useReducer, useCallback, useEffect } from 'react';
 import './App.css';
+import {isEmpty} from 'lodash'
 
 function App() {
     const inputRef = useRef()
+    const clearListButtonRef = useRef()
     const [items, dispatch] = useReducer((state, action) => {
         switch (action.type) {
             case "add":
                 return [...state, {name: action.name, id: state.length}]
             case "remove":
                 return state.filter(item => item.id !== action.id)
+            case "clear":
+                return []
             default:
                 return state
         }
@@ -26,6 +30,18 @@ function App() {
         dispatch({type: "remove", id: id})
     }, [])
 
+    useEffect(() => {
+        if (isEmpty(items)) {
+            clearListButtonRef.current.setAttribute("disabled", true)
+        } else {
+            clearListButtonRef.current.removeAttribute("disabled")
+        }
+    }, [items])
+
+    const handleClearList = useCallback(() => {
+        dispatch({type: "clear"})
+    }, [])
+
     return (
         <div style={{padding: "20px"}}>
             <form onSubmit={handleSubmit}>
@@ -35,11 +51,14 @@ function App() {
                     style={{minWidth: "250px"}}
                 />
             </form>
+            <br/>
+            <button ref={clearListButtonRef} onClick={handleClearList}>Clear List</button>
             <ul>
                 {items.map(item => {
                     return (
                         <li key={item.id}>{item.name} &nbsp;
-                            <button key={item.id} onClick={() => handleRemoveItem(item.id)}>remove
+                            <button key={item.id} onClick={() => handleRemoveItem(item.id)}>
+                                remove
                             </button>
                         </li>
                     )
